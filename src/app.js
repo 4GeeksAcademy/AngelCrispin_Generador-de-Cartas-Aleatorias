158cc295333d8872627cfb54b64dffd233f41c46
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-08-05 04:56:44 +0000

    Task Finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,53 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+var suits = ["♦", "♥", "♠", "♣"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+
+  var newCard = document.getElementById("newCard");
+  newCard.addEventListener("click", generateCard);
+
+  setInterval(generateCard, 10000);
+
+  document.querySelectorAll(".config").forEach(function(x) {
+    x.addEventListener("change", function(e) {
+      changeWidthHeight();
+    });
+  });
 };
+
+function generateCard() {
+  let randSuit = Math.floor(Math.random() * 4);
+  let randNumber = Math.floor(Math.random() * 13) + 1;
+
+  let colorSuit = randSuit > 1 ? "black" : "red";
+  randNumber =
+    randNumber == 11
+      ? "J"
+      : randNumber == 12
+      ? "Q"
+      : randNumber == 13
+      ? "K"
+      : randNumber == 1
+      ? "A"
+      : randNumber;
+
+  var divCard = document.getElementById("cardPlay");
+  divCard.innerHTML = `
+          <div id="suitTopLeft" style="color:${colorSuit}">${suits[randSuit]}</div>
+          <div id="number">${randNumber}</div>
+          <div id="suitBottomRight" style="color:${colorSuit}">${suits[randSuit]}</div>
+    `;
+}
+
+function changeWidthHeight() {
+  var cardPlay = document.getElementById("cardPlay");
+
+  var cardWidth = document.getElementById("cardWidth").value;
+  var cardHeight = document.getElementById("cardHeight").value;
+
+  cardPlay.style.width = cardWidth == "" ? "400px" : cardWidth + "px";
+  cardPlay.style.height = cardHeight == "" ? "600px" : cardHeight + "px";
+}
